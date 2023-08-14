6ed392e8cb2455eec9a274b21d13ad3a85b834e7
Ivan Gentile <ivangentile@gmail.com>
2023-08-14 01:12:38 -0300

    Tercera Pre-entrega Gentile

--- main.js
+++ main.js
@@ -1,46 +1,88 @@
-const numeroAleatorio = Math.floor(Math.random() * 25) + 1;
+let numeroAleatorio = Math.floor(Math.random() * 25) + 1;
 const intentosMaximos = 5;
 let intentos = 0;
-const jugadores = []; // Array para almacenar
+let confettiInstance = null;
 
-const obtenerNombre = () => prompt('Ingresa tu nombre:');
+const adivinaForm = document.getElementById('adivina-form');
 
-function agregarJugador(nombre) {
-    jugadores.push({ nombre, intentos });
+function mostrarMejoresPuntuaciones() {
+    const puntuacionesLista = document.getElementById('puntuaciones-lista');
+    puntuacionesLista.innerHTML = '';
+
+    const mejoresPuntuaciones = JSON.parse(localStorage.getItem('mejoresPuntuaciones')) || [];
+
+    mejoresPuntuaciones.forEach((puntaje, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${index + 1}. ${puntaje.nombre} - ${puntaje.intentos} intentos`;
+        puntuacionesLista.appendChild(listItem);
+    });
 }
 
-function adivinaElNumero() {
-    const numero = parseInt(prompt('Ingresa un número entre 1 y 25:'));
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarMejoresPuntuaciones();
+});
+
+adivinaForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const numero = parseInt(document.getElementById('numero').value);
 
     if (isNaN(numero) || numero < 1 || numero > 25) {
-        console.log('Por favor, ingresa un número válido entre 1 y 25.');
-        adivinaElNumero(); // Pedimos un nuevo intento al jugador
+        Swal.fire({
+            icon: 'error',
+            title: 'Número inválido',
+            text: 'Por favor, ingresa un número válido entre 1 y 25.'
+        });
         return;
     }
 
     intentos++;
 
     if (numero === numeroAleatorio) {
-        console.log(`¡Felicidades ${jugadores[jugadores.length - 1].nombre}! Adivinaste el número ${numeroAleatorio} en ${intentos} intentos.`);
-        alert(`¡Felicidades ${jugadores[jugadores.length - 1].nombre}! Adivinaste el número en ${intentos} intentos.`);
+        confettiInstance = new ConfettiGenerator({ target: 'confetti-canvas' });
+        confettiInstance.render();
+
+        const puntaje = { nombre, intentos };
+        let mejoresPuntuaciones = JSON.parse(localStorage.getItem('mejoresPuntuaciones')) || [];
+        mejoresPuntuaciones.push(puntaje);
+        mejoresPuntuaciones.sort((a, b) => a.intentos - b.intentos);
+        localStorage.setItem('mejoresPuntuaciones', JSON.stringify(mejoresPuntuaciones));
+
+        Swal.fire({
+            icon: 'success',
+            title: `¡Felicidades ${nombre}!`,
+            text: `Adivinaste el número ${numeroAleatorio} en ${intentos} intentos.\n¡Tus mejores puntuaciones están disponibles en la sección "Mis Mejores Puntuaciones"!`,
+        }).then(() => {
+            confettiInstance.clear();
+            mostrarMejoresPuntuaciones();
+        });
     } else {
         if (intentos === intentosMaximos) {
-            console.log(`Lo siento ${jugadores[jugadores.length - 1].nombre}, has superado el límite de intentos. El número era ${numeroAleatorio}.`);
-            alert(`Lo siento ${jugadores[jugadores.length - 1].nombre}, has superado el límite de intentos. El número era ${numeroAleatorio}.`);
+            Swal.fire({
+                icon: 'warning',
+                title: 'Límite de intentos alcanzado',
+                text: `Lo siento ${nombre}, has superado el límite de intentos. El número era ${numeroAleatorio}.`,
+                showCancelButton: true,
+                confirmButtonText: 'Nuevo juego',
+                cancelButtonText: 'Salir',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    intentos = 0;
+                    numeroAleatorio = Math.floor(Math.random() * 25) + 1;
+                    mostrarMejoresPuntuaciones();
+                } else {
+                    window.close();
+                }
+            });
         } else {
             let mensajeFallo = '¡Fallaste! Sigue intentando.';
-            if (numero < numeroAleatorio) {
-                mensajeFallo += ' El número es mayor.';
-            } else {
-                mensajeFallo += ' El número es menor.';
-            }
-            alert(mensajeFallo); // Mensaje de fallo
-            adivinaElNumero(); // Pedimos un nuevo intento al jugador
+            mensajeFallo += numero < numeroAleatorio ? ' El número es mayor.' : ' El número es menor.';
+            Swal.fire({
+                icon: 'error',
+                title: 'Intento fallido',
+                text: mensajeFallo,
+            });
         }
     }
-}
-
-// Iniciar el juego
-const nombreJugador = obtenerNombre();
-agregarJugador(nombreJugador);
-adivinaElNumero();
+});
